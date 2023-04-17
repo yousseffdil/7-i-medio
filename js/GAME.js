a561d077fa24f7fcacb0560980b3488a962190e7
yousseffdil <fdilyoussef@gmail.com>
2023-04-17 23:46:21 +0200

    separar vista

--- js/GAME.js
+++ js/GAME.js
@@ -1,12 +1,27 @@
 /**
- * Es representa una classe anomenada Game.
- * */
+ * Representa una classe anomenada Game.
+ * @class
+ */
 class Game {
+  /**
+   * Crea una nova instància de la classe Game.
+   * @constructor
+   * @param {Deck} deck - La baralla de cartes que es farà servir en el joc.
+   * @param {Player} player - El jugador que juga el joc.
+   * @param {Computer} computer - La màquina que juga el joc.
+   * @param {Main} Main - La classe principal per executar el joc.
+   */
   constructor(deck, player, computer, Main) {
     this.deck = deck;
     this.player = player;
     this.computer = computer;
   }
+
+  /**
+   * Inicia el joc.
+   * barrejant la baralla.
+   * reiniciant les mans del jugador i de la màquina, i repartint les cartes.
+   */
   start() {
     this.deck.shuffle();
     this.player.reset();
@@ -17,16 +32,22 @@ class Game {
     }
   }
 
-
+  /**
+   * Afegeix una carta a la mà d'un jugador.
+   * @param {Player} player - El jugador al qual s'ha d'afegir la carta.
+   */
   hit(player) {
     player.addCard(this.deck.pop());
   }
 
+  /**
+   * Controla el torn de la màquina.
+   * La qual seguirà agafant cartes fins que obtingui una puntuació de 7.5.
+   * O fins que decideixi plantar-se.
+   */
   computerTurn() {
     while (this.computer.score < 7.5 && this.computer.cards.length < 5) {
       if (this.computer.score < 7.5) {
-        this.hit(this.computer);
-      } else if (this.computer.score >= 7.5 && this.computer.score < 7.5 && this.computer.cards.length === 2) {
         this.hit(this.computer);
       } else if (this.computer.score >= 7.5 && this.computer.score < 7.5) {
         const randomNumber = Math.random();
@@ -41,7 +62,10 @@ class Game {
     }
   }
 
-
+  /**
+   * Retorna el resultat final del joc.
+   * @return {Object} Un objecte que conté el resultat final del joc.
+   */
   getResult() {
     if (this.player.score > 7.5) {
       const text = 'El jugador pierde';
@@ -66,5 +90,4 @@ class Game {
     }
   }
 }
-
 export {Game};
--- js/vista.js
+++ js/vista.js
@@ -1,31 +1,109 @@
-export function getStartButton() {
-  return document.getElementById('start');
-}
+/**
+ * Una classe que representa la vista del joc
+ * */
+export class Vista {
+  /**
+   * Crea un objecte vista.
+   * @constructor
+   * @param{Object} game -L'objecte del joc
+   * */
+  constructor(game) {
+    this.game = game;
+    this.startButton = document.getElementById('start');
+    this.hitButton = document.getElementById('hit');
+    this.standButton = document.getElementById('stand');
+    this.resultDiv = document.getElementById('result');
+    this.playerCards = document.getElementById('player-cards');
+    this.playerScore = document.getElementById('player-score');
+    this.computerCards = document.getElementById('computer-cards');
+    this.computerScore = document.getElementById('computer-score');
+  }
 
-export function getHitButton() {
-  return document.getElementById('hit');
-}
+  /**
+   * Inicialitza la vista afegint Listeners i  restablir el joc.
+   * */
+  init() {
+    this.startButton.addEventListener('click', () => {
+      this.game.start();
+      this.disableHitStandButtons();
+      this.enableHitStandButtons();
+      this.renderGame();
+    });
 
-export function getStandButton() {
-  return document.getElementById('stand');
-}
+    this.hitButton.addEventListener('click', () => {
+      this.game.hit(this.game.player);
+      this.renderGame();
+    });
 
-export function getResultDiv() {
-  return document.getElementById('result');
-}
+    this.standButton.addEventListener('click', () => {
+      this.disableHitStandButtons();
+      this.game.computerTurn();
+      this.renderGame();
+      this.renderResult();
+    });
 
-export function getPlayerCards() {
-  return document.getElementById('player-cards');
-}
+    this.reset();
+  }
 
-export function getPlayerScore() {
-  return document.getElementById('player-score');
-}
+  /**
+   * Representa l'estat actual del joc en la pantalla.
+   * */
+  renderGame() {
+    this.playerCards.innerHTML = '';
+    for (const card of this.game.player.cards) {
+      this.playerCards.innerHTML += card.getHTML();
+    }
+    if (this.game.player.score > 7.5) {
+      alert('Mi homie te has pasado');
+      this.disableHitStandButtons();
+    }
+    this.playerScore.textContent = this.game.player.score;
 
-export function getComputerCards() {
-  return document.getElementById('computer-cards');
-}
+    this.computerCards.innerHTML = '';
+    for (const card of this.game.computer.cards) {
+      this.computerCards.innerHTML += card.getHTML();
+    }
+    this.computerScore.textContent = this.game.computer.score;
+  }
 
-export function getComputerScore() {
-  return document.getElementById('computer-score');
+  /**
+   * Representa el resultat del joc a la pantalla.
+   * */
+  renderResult() {
+    const result = this.game.getResult();
+    this.resultDiv.textContent = result;
+  }
+
+  /**
+   * Desactuva el boto 'hit' i 'stand'.
+   * */
+  disableHitStandButtons() {
+    this.hitButton.disabled = true;
+    this.standButton.disabled = true;
+  }
+  /**
+   * Activa el boto 'hit' i 'stand'.
+   * */
+  enableHitStandButtons() {
+    this.hitButton.disabled = false;
+    this.standButton.disabled = false;
+  }
+  /**
+   * Activa el boto 'start'.
+   * */
+  enableStartButton() {
+    this.startButton.disabled = false;
+  }
+  /**
+   * Reinicia el joc.
+   * */
+  reset() {
+    this.game.deck.shuffle();
+    this.game.player.reset();
+    this.game.computer.reset();
+    this.resultDiv.textContent = '';
+    this.disableHitStandButtons();
+    this.enableStartButton();
+    this.renderGame();
+  }
 }
