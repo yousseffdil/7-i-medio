2babdd412c8fe70847d999a9fd08e340ca279b4c
yousseffdil <fdilyoussef@gmail.com>
2023-04-18 11:05:21 +0200

    l

--- test/vista.test.js
+++ test/vista.test.js
@@ -1,1 +1,12 @@
-
+import { Card } from './card.js'
+// Tests that the game ends and the result is calculated correctly. 
+it('test_game_ends_and_result_is_calculated', () => {
+    const deck = new Deck();
+    const player = new Player();
+    const computer = new Computer();
+    const game = new Game(deck, player, computer);
+    player.score = 8;
+    computer.score = 7;
+    const result = game.getResult();
+    expect(result.value).toBe('El jugador pierde');
+});
